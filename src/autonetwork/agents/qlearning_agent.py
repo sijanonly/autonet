be2aa47384b83a1627528ca07d72d8d7ecc04b79
sijanonly <sijanonly@gmail.com>
2020-05-26 16:58:35 +0200

    update q learning agent with grid movements around parameter space

--- src/autonetwork/agents/qlearning_agent.py
+++ src/autonetwork/agents/qlearning_agent.py
@@ -1,29 +1,31 @@
 import random
 import numpy as np
 
-from .agent import Agent
+import random
 
-
-class Agent(object):
+class Agent(object):  
+        
     def __init__(self, actions):
         self.actions = actions
         self.num_actions = len(actions)
 
     def act(self, state):
         raise NotImplementedError
-
-
+        
 class QLearningAgent(Agent):
-    def __init__(self, actions, epsilon=0.1, alpha=0.5, gamma=0.9):
+    
+    def __init__(self, actions, epsilon=0.1, alpha=0.8, gamma=0.95):
         super(QLearningAgent, self).__init__(actions)
-
+        
         ## TODO 1
         ## Initialize empty dictionary here
         ## In addition, initialize the value of epsilon, alpha and gamma
-        self.Q = {}
-        self.epsilon = epsilon
-        self.gamma = gamma
-        self.alpha = alpha
+        self.Q={}
+        self.epsilon=epsilon
+        self.gamma=gamma
+        self.alpha=alpha
+        
+        
 
     def stateToString(self, state):
         mystring = ""
@@ -32,40 +34,55 @@ class QLearningAgent(Agent):
         else:
             for digit in state:
                 mystring += str(digit)
-        return mystring
-
+        return mystring  
+     
     def getQ(self, state, action):
         return self.Q.get((state, action), 0.0)
-
-    def act(self, state):
-
+    
+    def act(self, state, epsilon=None):
+        
+        if epsilon:
+            self.epsilon = epsilon
+        
         stateStr = self.stateToString(state)
         exp_exp_tradeoff = random.uniform(0, 1)
-        #         print('trade is', exp_exp_tradeoff)
-        ## If this number > greater than epsilon --> exploitation
-        # (taking the biggest Q value for this state)
+#         print('trade is', exp_exp_tradeoff)
+        ## If this number > greater than epsilon --> exploitation (taking the biggest Q value for this state)
+#         dynamic exploration : increasing.
+# reducing the epsilon with increasing time (more exploration at the beginning)
+# random initial state:
         if exp_exp_tradeoff > self.epsilon:
-            q_values = [self.getQ(stateStr, a) for a in self.actions]
-            #             print('q values', q_values)
-            action = np.argmax(q_values)
+#             q_values = [self.getQ(stateStr,a) for a in self.actions]
+# #             print('q values', q_values)
+#             action = np.argmax(q_values)
+            q_values = {a: self.getQ(stateStr,a) for a in self.actions}
+            max_q = max(q_values.values())
+            # if we have multiple state with max q values, we choose randomly
+            actions_with_max_q = [a for a, q in q_values.items() if q == max_q]
+            action = np.random.choice(actions_with_max_q)
 
         # Else doing a random choice --> exploration
         else:
+#             action = env.action_space.sample()
             actions = self.actions
             action = np.random.randint(0, len(actions))
-
-        #         print('action is ', action)
+        
+#         print('action is ', action)
         return action
-
+    
     def learn(self, state1, action1, reward, state2, done):
+        """
+        Q[state, action] = Q[state, action] + lr * (reward + gamma * np.max(Q[new_state, :]) — Q[state, action])
+        """
         state1Str = self.stateToString(state1)
         state2Str = self.stateToString(state2)
-
-        oldv = self.Q.get((state1Str, action1), None)
-        if oldv is None:
-            self.Q[(state1Str, action1)] = reward
+        
+        old_q = self.Q.get((state1Str, action1), None)
+        if old_q is None:
+            self.Q[(state1Str, action1)] = reward 
         else:
-            best_a = max([self.getQ(state2Str, a) for a in self.actions])
-            temp = self.alpha * (reward + self.gamma * (best_a - oldv))
-            self.Q[(state1Str, action1)] += temp
+            best_a= max([self.getQ(state2Str, a) for a in self.actions])
+            temp=self.alpha*(reward+self.gamma*best_a-old_q)
+            self.Q[(state1Str, action1)]+=temp
         return self.Q
+        
--- src/autonetwork/environments/single_parameter.py
+++ src/autonetwork/environments/single_parameter.py
@@ -12,17 +12,16 @@ class Environment(object):
     def step(self):
         raise NotImplementedError("Inheriting classes must override step")
 
-
-class ActionSpace(Environment):
+class ActionSpace(object):
     def __init__(self, actions):
         self.actions = actions
         self.n = len(actions)
-
+    
     def sample(self):
         return np.random.randint(0, self.n)
-
-
+    
 class TwoParaEnv(Environment):
+    
     def __init__(self, R):
         self.shape = (9, 9)
         # define state and action space
@@ -35,56 +34,83 @@ class TwoParaEnv(Environment):
     def _convert_state(self, state):
         converted = np.unravel_index(state, self.shape)
         return np.asarray(list(converted), dtype=np.float32)
-
+    
     def reset(self):
         self.s = 0
         self.is_reset = True
         return self._convert_state(self.s)
-
+    
+    def _check_valid_move(self, state, action, current_pos_x):
+        next_idx, next_idy = np.unravel_index(state, self.shape)
+        is_state_valid = state >0 or state < 9*9-1
+        is_valid_move = (action == 1 and next_idx < current_pos_x) or (action==3 and next_idx > current_pos_x)
+        
+        return is_state_invalid and is_invalid_move
+    
     def update_state(self, state, action):
-        # UP = 0
-        # RIGHT = 1
-        # DOWN = 2
-        # LEFT = 3
-
+        """
+        Update state after action. If the moves are invalid, we will accumulate negative reward for each
+        invalid move
+        """
+             # UP = 0
+            # RIGHT = 1
+            # DOWN = 2
+            # LEFT = 3
+#         print('inside update state: current state', state)
+#         print('insdie update state : current action', action)
+        invalid_move_reward = 0
+        new_action = np.random.randint(0, self.action_space.n)
         x, y = np.unravel_index(state, self.shape)
-        around_map = [state - 9, state + 1, state + 9, state - 1]
+        around_map = [state-9, state+1, state+9, state-1]
         selected = around_map[action]
         if selected < 0:
+            invalid_move_reward -= 5
             return state
-
-        if selected > (9 * 9 - 1):
+#             return self.update_state(state, new_action)
+        
+        if selected > (9*9-1):
+            invalid_move_reward -= 5
+#             return self.update_state(state, new_action)
             return state
-
+        
         next_idx, next_idy = np.unravel_index(selected, self.shape)
-        if action == 1 and next_idx > x or action == 3 and next_idx < x:
+        # if action is right and unravel idx is greather than current idx, it will to go next row,(stop there)
+        if action == 1 and next_idx > x or action==3 and next_idx < x:
+            invalid_move_reward -= 5
             return state
-
+#             return self.update_state(state, new_action)
+        
+#         if self._check_valid_move(selected, action, x):
         return selected
-
+#         else:
+#             return self.update_state(state, new_action)
+    
     def observe_reward(self, state, prev_state, action):
         if state == prev_state:
-            return 0
+            return -5
         return self.R[state]
-
+        
     def is_done(self, state):
         max_reward_idx = self.R.argmax()
         return state == max_reward_idx
-
+        
     def step(self, action):
         s_prev = self.s
         self.s = self.update_state(self.s, action)
         reward = self.observe_reward(self.s, s_prev, action)
+        print('current step reward', reward)
         done = self.is_done(self.s)
-        return (self._convert_state(self.s), reward, done, "")
-
-    def render(self, mode="rgb_array", close=False):
+        return (self._convert_state(self.s), reward, done, '')
+    
+    def render(self, mode='rgb_array', close=False):
         if close:
             return
         final = np.argmax(self.R)
-        if mode == "rgb_array":
+        if mode == 'rgb_array':
             maze = np.zeros((9, 9))
-            maze[np.unravel_index(self.s, self.shape)] = 2.0
-            maze[np.unravel_index(final, self.shape)] = 0.5
+#             print('current S is', self.s)
+#             maze = np.reshape(self.R, (-1, 9))
+            maze[np.unravel_index(self.s, self.shape)] = -100.0
+            maze[np.unravel_index(final, self.shape)] = 200.0
             img = np.array(maze, copy=True)
             return img
